f081adcf0b5050df3e7119149c5a17f6f772e4ba
Micha Kops <micha.kops@gmail.com>
2017-02-26 18:57:44 +0100

    Karate updated to 0.2.0.

--- src/test/java/feature/user/UserManagementTest.java
+++ src/test/java/feature/user/UserManagementTest.java
@@ -2,7 +2,7 @@ package feature.user;
 
 import org.junit.runner.RunWith;
 
-import com.intuit.karate.Karate;
+import com.intuit.karate.junit4.Karate;
 
 @RunWith(Karate.class)
 public class UserManagementTest {
